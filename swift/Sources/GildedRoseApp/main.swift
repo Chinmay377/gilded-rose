7ff17b8ba4bd3c09f4608f6d5969d722bc5f1288
Chinmay Nayak <56029022+Chinmay377@users.noreply.github.com>
2022-03-01 22:52:58 +0530

    Update main.swift
    Updating the main file and using the factory class method for object creation.

--- swift/Sources/GildedRoseApp/main.swift
+++ swift/Sources/GildedRoseApp/main.swift
@@ -1,18 +1,9 @@
 import GildedRose
 
-let items = [
-    Item(name: "+5 Dexterity Vest", sellIn: 10, quality: 20),
-    Item(name: "Aged Brie", sellIn: 2, quality: 0),
-    Item(name: "Elixir of the Mongoose", sellIn: 5, quality: 7),
-    Item(name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80),
-    Item(name: "Sulfuras, Hand of Ragnaros", sellIn: -1, quality: 80),
-    Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 15, quality: 20),
-    Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 10, quality: 49),
-    Item(name: "Backstage passes to a TAFKAL80ETC concert", sellIn: 5, quality: 49),
-    // this conjured item does not work properly yet
-    Item(name: "Conjured Mana Cake", sellIn: 3, quality: 6)]
-
-let app = GildedRose(items: items);
+//let app = GildedRose(items: items);
+//let newItems = InventoryFactoryJSONResponse().getAllItems()
+let newItems = InventoryFactoryHardCoded().getAllItems()
+let app = GildedRose(items: newItems);
 
 var days = 2;
 if (CommandLine.argc > 1) {
@@ -22,7 +13,7 @@ if (CommandLine.argc > 1) {
 for i in 0..<days {
     print("-------- day \(i) --------");
     print("name, sellIn, quality");
-    for item in items {
+    for item in newItems {
         print(item);
     }
     print("");
