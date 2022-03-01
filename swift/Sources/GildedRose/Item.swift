edccfb7ea4a77f62bae8ee7ae81e7c8c62f4b0a7
Chinmay Nayak <56029022+Chinmay377@users.noreply.github.com>
2022-03-01 22:54:53 +0530

    Update Item.swift
    Updating the Item class with category extension and enum.

--- swift/Sources/GildedRose/Item.swift
+++ swift/Sources/GildedRose/Item.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 public class Item {
     var name: String
@@ -15,4 +16,36 @@ extension Item: CustomStringConvertible {
     public var description: String {
         return "\(name), \(sellIn), \(quality)"
     }
+}
+
+extension Item {
+    public var category: Category {
+        if self.name.contains("Aged Brie") {
+            return .agedBrie
+        }
+        if self.name.contains("Backstage passes") {
+            return .backStage
+        }
+        if self.name.contains("Sulfuras") {
+            return .sulfurace
+        }
+        if self.name.contains("Conjured") {
+            return .conjured
+        }
+        return .general
+    }
+}
+
+// MARK: 
+public enum Category {
+    case agedBrie // "Aged Brie" actually increases in Quality the older it gets
+    case backStage //"Backstage passes", like aged brie, increases in Quality as its SellIn value approaches
+    case sulfurace // "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    case conjured // "Conjured" items degrade in Quality twice as fast as normal items
+    case general // At the end of each day our system lowers both values for every item
+}
+
+public enum Constants: Int {
+    case maxQuality = 50 // Quality max value
+    case minQuality = 0 // Quality min value
 }
