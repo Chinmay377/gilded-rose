67881b374fb0d64d23fe6231282544b7190797b3
Chinmay Nayak <56029022+Chinmay377@users.noreply.github.com>
2022-03-01 23:34:37 +0530

    Update GildedRoseTests.swift
    Updating the test cases.

--- swift/Tests/GildedRoseTests/GildedRoseTests.swift
+++ swift/Tests/GildedRoseTests/GildedRoseTests.swift
@@ -6,10 +6,11 @@ import XCTest
 class GildedRoseTests: XCTestCase {
 
     func testFoo() {
-        let items = [Item(name: "foo", sellIn: 0, quality: 0)]
+        let items = [Item(name: "Backstage passes to a concert", sellIn: 0, quality: 0)]
         let app = GildedRose(items: items);
         app.updateQuality();
         
-        XCTAssertEqual("fixme", app.items[0].name);
+        XCTAssertEqual("Backstage passes to a concert", app.items[0].name);
+        XCTAssertEqual(app.items[0].category, .backStage);
     }
 }
